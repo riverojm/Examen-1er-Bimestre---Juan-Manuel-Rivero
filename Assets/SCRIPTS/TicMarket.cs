dc4718af6a7efd051dafab3ad42afebf042517e6
juanma rivero <45921725+riverojm@users.noreply.github.com>
2022-04-12 14:07:23 -0300

    Terminado con revisiones hechas

--- Assets/SCRIPTS/TicMarket.cs
+++ Assets/SCRIPTS/TicMarket.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class TicMarket : MonoBehaviour
 {
-//    El supermercado TicMarket nos pide que realicemos un programa para que sus clientes puedan calcular el costo de algunos productos que se encuentran en promoción
+    //    El supermercado TicMarket nos pide que realicemos un programa para que sus clientes puedan calcular el costo de algunos productos que se encuentran en promoción
 
-//Se pide que se ingrese por inspector:
-//. Los nombre de tres productos
-//.Los precios de cada producto
-//.La cantidad de unidades deseada de cada producto
+    //Se pide que se ingrese por inspector:
+    //. Los nombre de tres productos
+    //.Los precios de cada producto
+    //.La cantidad de unidades deseada de cada producto
 
-//El programa debe cumplir los siguientes requerimientos:
+    //El programa debe cumplir los siguientes requerimientos:
 
-//Debe calcularse un descuento del 20% sobre la compra de un producto si la cantidad de unidades del mismo supera las 3. 
+    //Debe calcularse un descuento del 20% sobre la compra de un producto si la cantidad de unidades del mismo supera las 3. 
 
-//El programa debe devolver un mensaje de error descriptivo si el precio del producto o la cantidad de unidades son menores a 1.
+    //El programa debe devolver un mensaje de error descriptivo si el precio del producto o la cantidad de unidades son menores a 1.
 
-//El programa debe devolver el total de la compra de los tres productos mostrando un mensaje que incluya el monto total sin descuento, el monto de los descuentos aplicados y el total con los descuentos aplicados.
+    //El programa debe devolver el total de la compra de los tres productos mostrando un mensaje que incluya el monto total sin descuento, el monto de los descuentos aplicados y el total con los descuentos aplicados.
 
     // Start is called before the first frame update
     public string nombreproducto1;
@@ -56,7 +56,9 @@ public class TicMarket : MonoBehaviour
         preciototalcondescuento = precioproducto1 + precioproducto2 + precioproducto3;
         float descuentoefectuado;
         descuentoefectuado = preciototalsindescuento - preciototalcondescuento;
-        Debug.Log("Monto total sin descuento: "+preciototalsindescuento+", Monto de los descuentos aplicados: "+descuentoefectuado+", Total con descuentos hechos: "+preciototalcondescuento+"");
+        float preciototalcondescuentofinal = descuentoefectuado;
+        descuentoefectuado = preciototalsindescuento - descuentoefectuado;
+        Debug.Log("Monto total sin descuento: "+preciototalsindescuento+", Monto de los descuentos aplicados: "+descuentoefectuado+", Total con descuentos hechos: "+preciototalcondescuentofinal+"");
     }
 
 
